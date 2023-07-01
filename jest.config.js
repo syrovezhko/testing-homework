1f6316d6241a4ff797220e59831532169df62dc6
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-07-01 19:07:11 +0300

    feat: set extra for `jest`

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
