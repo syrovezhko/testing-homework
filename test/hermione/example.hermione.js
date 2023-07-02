8577dbc65bba3381275e811043737962e4aa68cb
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-07-02 16:03:00 +0300

    feat: `burger hide` test hermiona

--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -27,4 +27,37 @@ describe('Общие требования:', async () => {
             });
         });
     }
+
+    it('4. На ширине меньше 576px навигационное меню должно скрываться за гамбургер', async ({ browser }) => {
+        await browser.setWindowSize(575, 500);
+        await browser.url(main_url);
+
+        const page = await browser.$('.Application');
+        await page.waitForExist();
+
+        const appToggler = await browser.$('.Application-Toggler');
+        const appMenu = await browser.$('.Application-Menu');
+
+        assert.equal(await appToggler.isDisplayed(), true, 'Гамбургер должен появиться');
+        assert.equal(await appMenu.isDisplayed(), false, 'Навигационное меню должно скрыться');
+    });
+
+    it('5. При выборе элемента из меню "гамбургера", меню должно закрываться', async ({ browser }) => {
+        await browser.setWindowSize(575, 500);
+        await browser.url(main_url);
+
+        const page = await browser.$('.Application');
+        await page.waitForExist();
+
+        const appToggler = await browser.$('.Application-Toggler');
+        const appMenu = await browser.$('.Application-Menu');
+
+        assert.equal(await appToggler.isDisplayed(), true, 'Гамбургер должен появиться');
+
+        await appToggler.click();
+        assert.equal(await appMenu.isDisplayed(), true, 'Меню должно открыться при клике на гамбургер');
+
+        await appMenu.click();
+        assert.equal(await appMenu.isDisplayed(), false, 'Меню должно закрыться при выборе элемента');
+    });
 });
