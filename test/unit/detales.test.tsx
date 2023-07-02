210914fee204a2a7be90dae82efbd13c89aa121b
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-07-02 12:43:33 +0300

    feat: `products info` test

---
+++ test/unit/detales.test.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider, useSelector } from "react-redux";
+import { createStore } from "redux";
+import { Product } from "./../../src/client/pages/Product";
+
+import userEvent from "@testing-library/user-event";
+import * as reduxHooks from "react-redux";
+// import * as routerHooks from "react-router";
+import routeData from "react-router";
+
+const initialState = {
+  cart: {},
+};
+
+function reducer(state = initialState, action: any) {
+  return state;
+}
+
+const initialStoreProduct = {
+  id: 0,
+  name: "Fantastic Keyboard",
+  description:
+    "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+  price: 133,
+  color: "olive",
+  material: "Wooden",
+};
+
+const store = createStore(reducer);
+
+describe("Product", () => {
+  it("products info", async () => {
+    jest.spyOn(reduxHooks, "useSelector").mockReturnValue(initialStoreProduct);
+
+    jest.mock("react-router-dom", () => ({
+      ...jest.requireActual("react-router-dom"), // use actual for all non-hook parts
+      useParams: () => ({
+        id: "0",
+      }),
+      useRouteMatch: () => ({ url: "/hw/store/catalog/0" }),
+    }));
+
+    const app = (
+      <BrowserRouter>
+        <Provider store={store}>
+          <Product />
+        </Provider>
+      </BrowserRouter>
+    );
+    const { container } = render(app);
+    expect(container.getElementsByClassName('ProductDetails-Name')[0].innerHTML).toEqual(initialStoreProduct.name)
+    expect(container.getElementsByClassName('ProductDetails-Description')[0].innerHTML).toEqual(initialStoreProduct.description)
+    expect(container.getElementsByClassName('ProductDetails-Price')[0].innerHTML).toEqual(`$${initialStoreProduct.price}`)
+    expect(container.getElementsByClassName('ProductDetails-Color')[0].innerHTML).toEqual(initialStoreProduct.color)
+    expect(container.getElementsByClassName('ProductDetails-Material')[0].innerHTML).toEqual(initialStoreProduct.material)
+    expect(container.getElementsByClassName('ProductDetails-AddToCart').length).toEqual(1)
+    expect(container).toMatchSnapshot
+  });
+});
